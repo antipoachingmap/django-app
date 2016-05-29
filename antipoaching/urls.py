d2fee6440783166acbd9b3cab6b96f18c746a633
ninebelowzero <richardjohnsilk@gmail.com>
2016-05-29 17:09:10 +0100

    Commente dout soem dead code.

--- antipoaching/urls.py
+++ antipoaching/urls.py
@@ -10,4 +10,4 @@ app_name = 'events'
     # url(r'^$', views.events, name='index'),
     # url(r'^(?P<movie_id>[0-9]+)/$', views.movie_detail, name='movie_detail'),
     # url('reviews', views.reviews, name='reviews_index'),
-]
+# ]
--- antipoaching/views.py
+++ antipoaching/views.py
@@ -40,6 +40,3 @@ class UserList(generics.ListAPIView):
 class UserDetail(generics.RetrieveAPIView):
 	queryset = User.objects.all()
 	serializer_class = UserSerializer
-
-
-
