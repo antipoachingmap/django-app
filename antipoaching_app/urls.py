d3b1f47df7f2fc689d8083ecf8a087ac3e109b70
ninebelowzero <richardjohnsilk@gmail.com>
2016-05-29 12:19:39 +0100

    Created empty Django project.

--- antipoaching_app/urls.py
+++ antipoaching_app/urls.py
@@ -1,4 +1,4 @@
-"""antipoachingapp URL Configuration
+"""antipoaching_app URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.9/topics/http/urls/
