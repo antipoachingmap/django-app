f71cd78111d021ed7bd260ef908a59ea7f14f422
mickyginger <mickyginger@gmail.com>
2016-05-29 15:04:45 +0100

    Removing event model

--- antipoaching/models.py
+++ antipoaching/models.py
@@ -4,18 +4,6 @@ from django.db import models
 from django_unixdatetimefield import UnixDateTimeField
 # Create your models here.
 
-SEVERITY_CHOICES = [('c', "critical"), ('w', "warning"), ('i', "info")]
-
-class Event(models.Model):
-	description = models.TextField(max_length=500)
-	severity = models.CharField(('severity'), choices=SEVERITY_CHOICES, default='i', max_length=1)
-
-
-	# email = models.EmailField(null=True, default="",blank=True)
-	# type = models.CharField(('type'), choices=USERTYPES, default='r', max_length=1)
-	# def __str__(self):
-	#     return self.name
-
 class Media(models.Model):
 	description = models.TextField()
 	format = models.CharField(blank=False, max_length=3)
