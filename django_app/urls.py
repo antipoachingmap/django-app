60069c65bef3c017d3b65945990b0edd0f1e1ffe
Will Melbourne <willmelbourne@gmail.com>
2016-05-29 18:03:25 +0100

    merge conflict

--- django_app/urls.py
+++ django_app/urls.py
@@ -1,18 +1,3 @@
-"""django_app URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.9/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
-"""
 from django.conf.urls import url, include
 from django.contrib import admin
 
@@ -20,17 +5,13 @@ from antipoaching import views as antipoachingviews
 from rest_framework import routers
 
 router = routers.DefaultRouter()
-# router.register(r'users', views.UserViewSet)
-# router.register(r'groups', views.GroupViewSet)
 router.register(r'v1/events', antipoachingviews.EventViewSet)
+router.register(r'v1/media', antipoachingviews.MediaViewSet)
 
 urlpatterns = [
     url(r'^', include(router.urls)),
-	url(r'^', include('antipoaching.urls')),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^admin/', admin.site.urls),
-    url(r'^users/$', antipoachingviews.UserList.as_view()),
-    url(r'^users/(?P<pk>[0-9]+)/$', antipoachingviews.UserDetail.as_view()),
 ]
 
 
