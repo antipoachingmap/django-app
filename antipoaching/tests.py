7b16d73aa2eb8e6df62a8c4b77d2bf0938f8490e
mickyginger <mickyginger@gmail.com>
2016-05-29 16:41:37 +0100

    Added working test

--- antipoaching/tests.py
+++ antipoaching/tests.py
@@ -1,14 +1,20 @@
 from django.test import TestCase
+from rest_framework.test import APIClient
 
 # Create your tests here.
 class MediaAPITests(TestCase):
 	def test_creating_media_object(self):
-		response = self.client.post('/media', {
-			description: 'A party parrot',
-			format: 'png',
-			timestamp: 1463322187,
-			filename: 'part_parrot',
-			filesize: 500123
+
+		client = APIClient()
+
+		response = client.post('/v1/media/', {
+			'description': 'A party parrot',
+			'format': 'png',
+			'timestamp': '2016-05-29T16:40',
+			'filename': 'part_parrot',
+			'filesize': 500123
 		})
 
-		self.assertEqual(response.status_code, 200)
+		print response.content
+
+		self.assertEqual(response.status_code, 201)
