06646b3bb6b3e0c378b736420703518faa76f61c
Will Melbourne <willmelbourne@gmail.com>
2016-05-29 18:12:35 +0100

    add authentication to classes

--- antipoaching/views.py
+++ antipoaching/views.py
@@ -1,11 +1,14 @@
 from rest_framework import viewsets
 from antipoaching.serializers import EventSerializer, MediaSerializer
 from .models import Event, Media
+from rest_framework import permissions
 
 class EventViewSet(viewsets.ModelViewSet):
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
     queryset = Event.objects.all()
     serializer_class = EventSerializer
 
 class MediaViewSet(viewsets.ModelViewSet):
+    permission_classes = (permissions.IsAuthenticated,)
     queryset = Media.objects.all()
     serializer_class = MediaSerializer
