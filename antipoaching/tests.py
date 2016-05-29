b721738a0804871d8087a5adb3c344dfecad1d88
ninebelowzero <richardjohnsilk@gmail.com>
2016-05-29 17:33:34 +0100

    Improved events list test.

--- antipoaching/tests.py
+++ antipoaching/tests.py
@@ -18,8 +18,12 @@ class EventTests(TestCase):
 		response = client.get(API + 'asteroids/')
 		self.assertEqual(response.status_code, 404)
 
-	def test_200_for_events(self):
+	def test_getting_list_of_events(self):
 		client = Client()
 		response = client.get(API + 'v1/events/')
 		self.assertEqual(response.status_code, 200)
+		self.assertTrue('count' in response.data)
+		self.assertTrue('next' in response.data)
+		self.assertTrue('previous' in response.data)
+		self.assertTrue('results' in response.data)
 
