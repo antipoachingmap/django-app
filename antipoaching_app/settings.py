7e728d716d8ecf7fc17d97a90b70415156893133
mickyginger <mickyginger@gmail.com>
2016-05-29 13:13:18 +0100

    Working on connecting to mongo

--- antipoaching_app/settings.py
+++ antipoaching_app/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 import os
+import mongoengine
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -78,10 +79,21 @@ WSGI_APPLICATION = 'antipoaching_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': '',
     }
 }
+
+SESSION_ENGINE = 'mongoengine.django.sessions'
+
+_MONGODB_USER = 'admin'
+_MONGODB_PASSWD = 'password123'
+_MONGODB_HOST = 'thehost'
+_MONGODB_NAME = 'thedb'
+_MONGODB_DATABASE_HOST = \
+    'mongodb://%s:%s@%s/%s' \
+    % (_MONGODB_USER, _MONGODB_PASSWD, _MONGODB_HOST, _MONGODB_NAME)
+
+mongoengine.connect(_MONGODB_NAME, host=_MONGODB_DATABASE_HOST)
 
 
 # Password validation
