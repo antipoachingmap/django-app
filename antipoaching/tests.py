977f57ded7b235f49eba523c6b066e5e07946e00
mickyginger <mickyginger@gmail.com>
2016-05-29 17:27:00 +0100

    Added test suite

--- antipoaching/tests.py
+++ antipoaching/tests.py
@@ -1,28 +1,93 @@
 from django.test import TestCase
 from rest_framework.test import APIClient
+import json
+
+from .models import Media
+
+client = APIClient()
+
+def create_media_object():
+	return Media.objects.create(
+		description='A party parrot',
+		format='png',
+		timestamp=1463322187,
+		filename='party_parrot',
+		filesize=500123
+	)
 
 # Create your tests here.
 class MediaAPITests(TestCase):
-	def test_creating_media_object(self):
-
-		client = APIClient()
+	def test_create_media_object(self):
 
 		response = client.post('/v1/media/', {
 			'description': 'A party parrot',
 			'format': 'png',
 			'timestamp': 1463322187,
-			'filename': 'part_parrot',
+			'filename': 'party_parrot',
 			'filesize': 500123
 		})
 
+		data = json.loads(response.content)
+
 		self.assertEqual(response.status_code, 201)
-		self.assertEqual(response.content, '{"id":1,"description":"A party parrot","format":"png","timestamp":1463322187,"filename":"part_parrot","filesize":500123}')
+		self.assertEqual(data['description'], 'A party parrot')
+		self.assertEqual(data['format'], 'png')
+		self.assertEqual(data['timestamp'], 1463322187)
+		self.assertEqual(data['filename'], 'party_parrot')
+		self.assertEqual(data['filesize'], 500123)
 
-	def test_listing_media_objects(self):
+	def test_list_media_objects(self):
 
-		client = APIClient()
+		create_media_object()
 
 		response = client.get('/v1/media/')
 
+		data = json.loads(response.content)
+
 		self.assertEqual(response.status_code, 200)
-		self.assertEqual(response.content, '[]')
+		self.assertEqual(len(data), 1)
+
+	def test_detail_media_object(self):
+
+		record = create_media_object()
+
+		response = client.get('/v1/media/' + str(record.id) + '/')
+
+		data = json.loads(response.content)
+
+		self.assertEqual(response.status_code, 200)
+		self.assertEqual(data['description'], 'A party parrot')
+		self.assertEqual(data['format'], 'png')
+		self.assertEqual(data['timestamp'], 1463322187)
+		self.assertEqual(data['filename'], 'party_parrot')
+		self.assertEqual(data['filesize'], 500123)
+
+
+	def test_update_media_object(self):
+
+		record = create_media_object()
+
+		response = client.put('/v1/media/' + str(record.id) + '/', {
+			'description': 'A party pangolin',
+			'format': 'png',
+			'timestamp': 1463322187,
+			'filename': 'party_pangolin',
+			'filesize': 500666
+		})
+
+		data = json.loads(response.content)
+
+		self.assertEqual(response.status_code, 200)
+		self.assertEqual(data['description'], 'A party pangolin')
+		self.assertEqual(data['format'], 'png')
+		self.assertEqual(data['timestamp'], 1463322187)
+		self.assertEqual(data['filename'], 'party_pangolin')
+		self.assertEqual(data['filesize'], 500666)
+
+	def test_delete_media_object(self):
+
+		record = create_media_object()
+
+		response = client.delete('/v1/media/' + str(record.id) + '/')
+		self.assertEqual(response.status_code, 204)
+
