ae8b269432b7c762f576e0d4874a9a3508630924
ninebelowzero <richardjohnsilk@gmail.com>
2016-05-29 15:34:59 +0100

    Wrote first tests for events.

--- antipoaching/tests.py
+++ antipoaching/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from django.test.utils import setup_test_environment
+from django.test import Client
+setup_test_environment()
 
-# Create your tests here.
+
+from .models import Event
+
+class EventTests(TestCase):
+
+	def test_403_at_root(self):
+		client = Client()
+		response = client.get('/')
+		self.assertEqual(response.status_code, 403)
+
+	def test_404_from_bad_url(self):
+		client = Client()
+		response = client.get('/asteroids')
+		self.assertEqual(response.status_code, 404)
