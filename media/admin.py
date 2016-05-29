51b5616434950ccd28f14ecb5a6681c08253d292
mickyginger <mickyginger@gmail.com>
2016-05-29 13:06:53 +0100

    Attempting mongo integration

--- media/admin.py
+++ media/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Media
+# from .models import Media
 
-admin.site.register(Media)
+# admin.site.register(Media)
--- media/models.py
+++ media/models.py
@@ -2,6 +2,7 @@ from __future__ import unicode_literals
 
 from django.db import models
 from mongoengine import *
+from django_unixdatetimefield import UnixDateTimeField
 
 # Create your models here.
 
@@ -9,5 +10,5 @@ class Media(Document):
   description = StringField(max_length=200)
   format = StringField(max_length=3)
   timestamp = UnixDateTimeField()
-  filesize = BigIntegerField()
+  filesize = IntField()
   data = BinaryField()
