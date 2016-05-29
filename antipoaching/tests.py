81a670890d5977604e797dd6851efaf0c38e250f
mickyginger <mickyginger@gmail.com>
2016-05-29 16:59:51 +0100

    Removed pagination

--- antipoaching/tests.py
+++ antipoaching/tests.py
@@ -17,3 +17,12 @@ class MediaAPITests(TestCase):
 
 		self.assertEqual(response.status_code, 201)
 		self.assertEqual(response.content, '{"id":1,"description":"A party parrot","format":"png","timestamp":1463322187,"filename":"part_parrot","filesize":500123}')
+
+	def test_listing_media_objects(self):
+
+		client = APIClient()
+
+		response = client.get('/v1/media/')
+
+		self.assertEqual(response.status_code, 200)
+		self.assertEqual(response.content, '[]')
